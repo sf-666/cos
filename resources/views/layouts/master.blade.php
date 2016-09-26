835fbbfbce48d6b3278784571b2616b9573779ed
sf-666 <sf-666@gmx.net>
2016-09-26 20:23:08 +0200

    Base Laravel 7

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>COS - @yield("title")</title>
+</head>
+
+<body>
+<div>Navigation</div>
+<hr/>
+@yield("content")
+
+</body>
+
+</html>
