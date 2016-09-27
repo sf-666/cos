8cb381529e999349d725b6ce26b2e078c794a0a6
sf-666 <sf-666@gmx.net>
2016-09-27 19:19:03 +0200

    extended routes

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,57 @@ Route::get('/news/{name?}', function ($name = "Autoliebhaber") {
    // return view('news');
    // return "<h1>test</h1>";
 });
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/news', function () {
+    return view('news');
+});
+
+Route::get('/most-wanted', function () {
+    return view('most-wanted');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
+
+Route::get('/newest-cars', function () {
+    return view('newest-cars');
+});
+
+Route::get('/car-finder', function () {
+    return view('car-finder');
+});
+
+Route::get('/part-finder', function () {
+    return view('part-finder');
+});
+
+Route::get('/contact-us', function () {
+    return view('contact-us');
+});
+
+Route::get('/about-us', function () {
+    return view('about-us');
+});
+
+Route::get('/my-profile', function () {
+    return view('my-profile');
+});
+
+Route::get('/my-shop', function () {
+    return view('my-shop');
+});
+
+Route::get('/logout', function () {
+    return view('logout');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
